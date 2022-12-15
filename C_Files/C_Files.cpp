92fe3d1386cc09c7c2250b1c7dca7c844b51cb19
Oleksandr Kapara <a.kapara2006@gmail.com>
2022-12-16 00:27:44 +0200

    Добавьте файлы проекта.

---
+++ C_Files/C_Files.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+int CountOfWords(const char* path, char begin);
+
+int main()
+{
+    const char* pathStatist = "C:\\examples.txt";
+    const char* pathText = "C:\\examples.txt";
+
+    FILE* statistics;
+
+    char begin;
+    cout << "Enter begin symbol: ";
+    cin >> begin;
+
+    if (fopen_s(&statistics, pathStatist, "w") != NULL)
+    {
+        cout << "NOT OPEN";
+    }
+
+    else
+    {
+        fprintf(statistics, "%s", "words: ");
+        fprintf(statistics, "%d", CountOfWords(pathText, begin));
+    }
+
+}
+
+
+
+int CountOfWords(const char* path, char begin)
+{
+    FILE* TEXT;
+
+    int space;
+    int words = 1;
+    bool isStart = false;
+
+    if (fopen_s(&TEXT, path, "r") != NULL)
+    {
+        cout << "Error";
+    }
+
+    else
+    {
+        while (!feof(TEXT))
+        {
+            space = fgetc(TEXT);
+
+            if (space == begin)
+            {
+                isStart = true;
+            }
+
+            if (isStart && space == ' ')
+            {
+                words++;
+            }
+        }
+    }
+
+    return words;
+}
